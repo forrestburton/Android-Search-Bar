cfd2133eee4fbde7777a5143ee9e6d3d103ceb55
Forrest Burton <burton.forrest10@gmail.com>
2020-08-15 17:10:24 -0700

    Added logos to ListView

--- app/src/main/java/com/example/testsearch/MainActivity.java
+++ app/src/main/java/com/example/testsearch/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.testsearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,7 +20,12 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView lvUniversities;
-    ArrayAdapter<String> universityAdapter;
+    CustomAdapter universityAdapter;
+
+    int images[] = {R.drawable.ucla_logo, R.drawable.ucsb_logo, R.drawable.yale_logo, R.drawable.uw_logo, R.drawable.stanford_logo};
+    String names[] = {"UCLA", "UCSB", "Yale", "UW", "Stanford"};
+    List<UniversityModel> universityList = new ArrayList<>();    //vector holding universities. Change to firebase for classes
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +33,67 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         lvUniversities = findViewById(R.id.lvUniversities);
-        List<String> universityList = new ArrayList<>();    //vector holding universities. Change to firebase
-        universityList.add("UCLA");
-        universityList.add("UCSB");
-        universityList.add("Yale");
-        universityList.add("Harvard");
-        universityList.add("Stanford");
 
-        universityAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, universityList);
+        for (int i = 0; i < names.length; i++)   //Add each university to the vector
+        {
+            UniversityModel universityModel = new UniversityModel(names[i], images[i]);
+            universityList.add(universityModel);
+        }
+
+        universityAdapter = new CustomAdapter(universityList, this);
         lvUniversities.setAdapter(universityAdapter);
-
         lvUniversities.setOnItemClickListener(this);
     }
 
+
+
+
+    public class CustomAdapter extends BaseAdapter {
+
+        private List<UniversityModel> universities;
+        private List<UniversityModel> universitiesFiltered;
+        private Context context;
+
+        public CustomAdapter(List<UniversityModel> universities, Context context) {
+            this.universities = universities;
+            this.universitiesFiltered = universities;  //SUS
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return universitiesFiltered.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.row_items, null);
+
+            ImageView universityView = view.findViewById(R.id.universityView);
+            TextView universityName = view.findViewById(R.id.universityName);
+
+            universityView.setImageResource(universitiesFiltered.get(position).getImage());
+            universityName.setText(universitiesFiltered.get(position).getName());
+
+
+            return view;
+        }
+    }
+
+
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String university = parent.getItemAtPosition(position).toString();
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {    //Recycler view onItemClick?
+        String university = ((UniversityModel)parent.getItemAtPosition(position)).getName();
         Toast.makeText(getApplicationContext(), "Selected: " + university, Toast.LENGTH_SHORT).show();
     }
 
---
+++ app/src/main/java/com/example/testsearch/UniversityModel.java
@@ -0,0 +1,29 @@
+package com.example.testsearch;
+
+public class UniversityModel {
+
+    private String name;
+    private int image;
+
+    public UniversityModel(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    //GETTERS and SETTERS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
